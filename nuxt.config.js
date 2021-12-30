477de776f09102038328c1ea84241a04b7a2a024
Vladislav Lyakishev <vlad169@yandex.ru>
2021-12-30 16:18:56 +0300

    [+] Init [+] Add auth

---
+++ nuxt.config.js
@@ -0,0 +1,83 @@
+import { configFirebase } from './config/firebase'
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'dashboard-sites',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'ant-design-vue/dist/antd.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '@/plugins/antd-ui'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/firebase'
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  // Firebase configuration
+  firebase: {
+    lazy: false,
+    config: configFirebase,
+    onFirebaseHosting: false,
+    terminateDatabasesAfterGenerate: true,
+    services: {
+      auth: {
+        initialize: {
+          onAuthStateChangedAction: 'auth/onAuthStateChanged'
+        },
+        ssr: true,
+        disableEmulatorWarnings: false
+      },
+      performance: true,
+      analytics: true,
+      remoteConfig: {
+        settings: {
+          fetchTimeoutMillis: 60000,
+          minimumFetchIntervalMillis: 43200000
+        },
+        defaultConfig: {
+          welcome_message: 'Welcome'
+        }
+      }
+    }
+  },
+
+}
---
+++ pages/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <a-row class="flex-center-middle" type="flex" justify="center" align="middle">
+    <a-col
+      :xs="{ span: 22 }"
+      :md="{ span: 10 }"
+      :lg="{ span: 6 }"
+    >
+      <AuthFormForm />
+    </a-col>
+  </a-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  beforeCreate () {
+    if (this.$store.getters['auth/isLoggedIn']) {
+      this.$router.replace('/')
+    }
+  }
+}
+</script>
+
+<style>
+  .flex-center-middle {
+    min-height: 100vh
+  }
+</style>
